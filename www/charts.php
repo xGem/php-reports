9a5d5ca894615aa899007076e4622fe5ef6e5d63
sergio <sergio.rod@gmail.com>
2017-11-06 11:49:50 -0300

    Fix code render in charts.php

--- www/charts.php
+++ www/charts.php
@@ -11,55 +11,55 @@
       <h4>PHP</h4>
       <pre>
         <code id="phpcode">// Accessing the first worksheet in the book ("Sheet1").
-        $sheet = $workbook->getWorksheets()->get(0);
-        $sheet->setName("Data");
+$sheet = $workbook->getWorksheets()->get(0);
+$sheet->setName("Data");
 
-        //Get the cells collection in the sheet.
-        $cells = $sheet->getCells();
+//Get the cells collection in the sheet.
+$cells = $sheet->getCells();
 
-        //Put some values into a cells of the Data sheet.
-        $cells->get("A1")->setValue("Empresa");
-        $cells->get("A2")->setValue("FENIE");
-        $cells->get("A3")->setValue("A2 ENERGIA");
-        $cells->get("A4")->setValue("NABALIA");
-        $cells->get("A5")->setValue("ALDRO");
-        $cells->get("A6")->setValue("FOENER");
-        $cells->get("A7")->setValue("CEMOI");
-        $cells->get("A8")->setValue("FOX");
-        $cells->get("A9")->setValue("ODF");
-        $cells->get("A10")->setValue("HOLALUZ");
-        $cells->get("A11")->setValue("WATIUM");
+//Put some values into a cells of the Data sheet.
+$cells->get("A1")->setValue("Empresa");
+$cells->get("A2")->setValue("FENIE");
+$cells->get("A3")->setValue("A2 ENERGIA");
+$cells->get("A4")->setValue("NABALIA");
+$cells->get("A5")->setValue("ALDRO");
+$cells->get("A6")->setValue("FOENER");
+$cells->get("A7")->setValue("CEMOI");
+$cells->get("A8")->setValue("FOX");
+$cells->get("A9")->setValue("ODF");
+$cells->get("A10")->setValue("HOLALUZ");
+$cells->get("A11")->setValue("WATIUM");
 
-        $cells->get("B1")->setValue("GWh");
-        $cells->get("B2")->setValue(41);
-        $cells->get("B3")->setValue(33);
-        $cells->get("B4")->setValue(28);
-        $cells->get("B5")->setValue(27);
-        $cells->get("B6")->setValue(27);
-        $cells->get("B7")->setValue(20);
-        $cells->get("B8")->setValue(19);
-        $cells->get("B9")->setValue(17);
-        $cells->get("B10")->setValue(17);
-        $cells->get("B11")->setValue(14);
+$cells->get("B1")->setValue("GWh");
+$cells->get("B2")->setValue(41);
+$cells->get("B3")->setValue(33);
+$cells->get("B4")->setValue(28);
+$cells->get("B5")->setValue(27);
+$cells->get("B6")->setValue(27);
+$cells->get("B7")->setValue(20);
+$cells->get("B8")->setValue(19);
+$cells->get("B9")->setValue(17);
+$cells->get("B10")->setValue(17);
+$cells->get("B11")->setValue(14);
 
-        $chartType = new ChartType();
-        $color = new Color();
+$chartType = new ChartType();
+$color = new Color();
 
-        //Create chart
-        $chartIndex = $sheet->getCharts()->add($chartType->COLUMN, 12, 1, 33, 12);
-        $chart = $sheet->getCharts()->get($chartIndex);
+//Create chart
+$chartIndex = $sheet->getCharts()->add($chartType->COLUMN, 12, 1, 33, 12);
+$chart = $sheet->getCharts()->get($chartIndex);
 
-        //Set properties of chart title
-        $chart->getTitle()->setText("TOP 10: Comercializadoras independientes de electricidad con mayor crecimiento");
-        $chart->getTitle()->getFont()->setBold(true);
-        $chart->getTitle()->getFont()->setSize(12);
+//Set properties of chart title
+$chart->getTitle()->setText("TOP 10: Comercializadoras independientes de electricidad con mayor crecimiento");
+$chart->getTitle()->getFont()->setBold(true);
+$chart->getTitle()->getFont()->setSize(12);
 
-        //Set properties of nseries
-        $chart->getNSeries()->add("Data!B2:B11", true);
-        $chart->getNSeries()->setCategoryData("Data!A2:A11");
+//Set properties of nseries
+$chart->getNSeries()->add("Data!B2:B11", true);
+$chart->getNSeries()->setCategoryData("Data!A2:A11");
 
-        //Set the legend invisible
-        $chart->setShowLegend(false);</code>
+//Set the legend invisible
+$chart->setShowLegend(false);</code>
         <button class="btn" data-clipboard-target="#phpcode" style="position:absolute;right:10px">Copy code <img class="clippy" width="13" src="images/clippy.svg" alt="Copy to clipboard"></button>
         <br/>
       </pre>
